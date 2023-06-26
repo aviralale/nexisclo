3826fdd935a20b3d743639f3f6d5be928e47798e
Aviral Ale <aviralale@gmail.com>
2023-06-26 22:48:01 +0545

    undo(1)

--- build_files.sh
+++
@@ -1,4 +0,0 @@
-echo " BUILD START"
-python3.11.4 -m pip install -r requirements.txt
-python3.11.4 manage.py collectstatic --noinput --clear
-echo " BUILD END" 
--- nexisclo/wsgi.py
+++ nexisclo/wsgi.py
@@ -12,7 +12,3 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nexisclo.settings')
-
-application = get_wsgi_application()
-
-app = application
